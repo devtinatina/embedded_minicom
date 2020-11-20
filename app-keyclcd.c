5792fc5497734dd7df634e1dc1ea3626c687a664
devtinatina <53737471+devtinatina@users.noreply.github.com>
2020-11-20 17:11:36 +0900

    fix app-keyclcd.c

--- app-keyclcd.c
+++ app-keyclcd.c
@@ -12,23 +12,34 @@ int main(void)
     int fd_key, fd_clcd;
     fd_key = open("/dev/key", O_RDWR);
     fd_clcd = open("/dev/clcd", O_RDWR);
+
     int value;
     char ch;
+    const char *blank = "                ";
+
     int pos = 17;
     ioctl(fd_clcd, LCD_INIT);
+
     while (1)
     {
         read(fd_key, &value, sizeof(int));
-        if (value == 11)          // # : exit break;
-            else if (value == 10) // * : next line {
-                ioctl(fd_clcd, LCD_SET_POS, pos);
-        pos = (pos == 17) ? 1 : 17;
+        if (value == 11)
+            break;            // # : exit break;
+        else if (value == 10) // * : next line
+        {
+            printf("set pos : %d\n", pos);
+            ioctl(fd_clcd, LCD_SET_POS, pos);
+            write(fd_clcd, blank, 16);
+            ioctl(fd_clcd, LCD_SET_POS, pos);
+            pos = (pos == 17) ? 1 : 17;
+        }
+        else
+        {
+            ch = value + 0;
+            write(fd_clcd, &ch, sizeof(char));
+        }
     }
-    else
-    {
-    }
-}
-close(fd_key);
-close(fd_clcd);
-return 0;
+    close(fd_key);
+    close(fd_clcd);
+    return 0;
 }
